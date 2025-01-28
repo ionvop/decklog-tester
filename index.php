56eb8b4c6ed9fdbf15482facca7d3900892322ff
ionvop <ionvop@gmail.com>
2025-01-29 01:09:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
 $data = json_decode($_POST["data"], true);
+echo "Processing data...<br>";
+ob_flush();
+flush();
 
 foreach ($data["deckImages"] as $key => $value) {
     $data["deckImages"][$key]["data"] = "data:image/png;base64," . base64_encode(file_get_contents($data["deckImages"][$key]["data"]));
-    echo $data["deckImages"][$key]["name"] . " done<br>";
+    $max = count($data["deckImages"]);
+    $current = $key + 1;
+    echo "{$current}/{$max} {$data['deckImages'][$key]['name']} done<br>";
+    ob_flush();
     flush();
 }
 
